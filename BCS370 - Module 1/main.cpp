ed1f606c0babbfaad272278176c8a3483a541088
hubbda <hubbda@farmingdale.edu>
2022-02-07 19:23:28 -0500

    Initial Commit

---
+++ BCS370 - Module 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BCS370 - Module 1
+//
+//  Created by Devon Hubbard on 2/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
